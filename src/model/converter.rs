649b526335880c2a39b25c7d5d1473b8833a4a84
Henrik Zenkert <henrik.zenkert@gmail.com>
2023-10-19 16:40:36 +0200

    not working with loading sqlite db

--- src/model/converter.rs
+++ src/model/converter.rs
@@ -1,16 +1,20 @@
 use std::{error::Error, fmt::format, fs::File, path::Path};
 
 use csv::{Reader, StringRecord, StringRecordsIter};
+use rusqlite::Connection;
 
 use crate::error::log;
 
 use super::{database::Database, datarow::DataRow};
 
-pub(crate) fn database_from_csv(path: &Path) -> crate::error::AppResult<Database> {
+pub(crate) fn database_from_csv(
+    path: &Path,
+    connection: Connection,
+) -> crate::error::AppResult<Database> {
     let mut csv = csv::Reader::from_path(path)?;
     let table_name = Database::get_table_name(path).unwrap();
     let table_names = vec![table_name.to_string()];
-    let mut database = Database::new(table_names)?;
+    let mut database = Database::new(table_names, connection)?;
     let mut queries = String::new();
     let query = build_create_table_query(&mut csv, table_name).unwrap();
     queries.push_str(&query);
@@ -71,35 +75,18 @@ fn build_value_query(record: &StringRecord) -> String {
         .map(|s| format!("'{}'", s.replace('\'', "XXXXXXXXXXX")))
         .collect::<Vec<String>>()
         .join(",");
-    return format!("({})", row);
+    format!("({})", row)
 }
 
-pub(crate) fn database_from_sqlite(path: &Path) -> crate::error::AppResult<Database> {
-    todo!()
-    // let mut csv = csv::Reader::from_path(path)?;
-    // let table_name = Database::get_table_name(path).unwrap();
-    // let table_names = vec![table_name.to_string()];
-    // let mut database = Database::new(table_names)?;
-    // let funs = vec![
-    //     Database::build_create_table_query,
-    //     Database::build_add_data_query,
-    // ];
-    // let mut queries = String::new();
-    // // queries.
-    // for fun in funs {
-    //     let query = fun(&mut csv, table_name).unwrap();
-    //     queries.push_str(&query);
-    //     // database.execute(&query, ())?;
-    // }
-
-    // database.execute_batch(&queries)?;
-    // let query =
-    //     "SELECT rowid FROM sqlite_master WHERE type='table' ORDER BY rowid LIMIT 1;".to_string();
-    // let table_idx: usize = database
-    //     .connection
-    //     .query_row(&query, [], |row| row.get(0))?;
-    // database.current_table_idx = table_idx;
-    // Ok(database)
+pub(crate) fn database_from_sqlite(connection: Connection) -> crate::error::AppResult<Database> {
+    let query = format!(
+        "SELECT name FROM sqlite_master WHERE type='table' AND rowid='{}';",
+        0
+    );
+    //         SELECT name FROM sqlite_master WHERE type='table' AND rowid=0;,
+    let table_name = connection.query_row(&query, [], |row| row.get(0))?;
+    let database = Database::new(vec![table_name], connection)?;
+    Ok(database)
 }
 
 pub(crate) fn build_create_table_query(
@@ -137,40 +124,3 @@ pub(crate) fn get_headers_for_query(
         .join(", ");
     Ok(columns)
 }
-// pub(crate) fn build_add_data_query<'a>(
-//     records: &StringRecordsIter<'a, File>,
-//     table_name: &str,
-//     columns: &str,
-//     limit: usize,
-// ) -> Result<(String, bool), Box<dyn Error>> {
-//     let mut i = 0;
-//     let mut items = Vec::new();
-//     for record in records {
-//         if i == limit {
-//             break;
-//         }
-//         if let Ok(record) = record {
-//             log(format!("record: {:?}", record));
-//             let row = record
-//                 .iter()
-//                 .map(|s| format!("'{}'", s))
-//                 .collect::<Vec<String>>()
-//                 .join(",");
-//             let mut result = "(".to_string();
-//             result.push_str(&row);
-//             result.push(')');
-//             items.push(result);
-//             i += 1;
-//         } else {
-//             break;
-//         }
-//     }
-//     let mut values = items.join(", ");
-//     values.push(';');
-
-//     let query = format!(
-//         "INSERT INTO '{}' ({}) VALUES {};",
-//         table_name, columns, values
-//     );
-//     Ok((query, i != limit))
-// }
