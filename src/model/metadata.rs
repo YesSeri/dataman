2ec93a33b65fd7e123d4e2054fddb1b0fd6c366c
Henrik <henrik.zenkert@gmail.com>
2024-07-14 12:41:52 +0200

    added tests to metadata table

--- src/model/metadata.rs
+++ src/model/metadata.rs
@@ -86,3 +86,110 @@ fn populate_table_of_tables(conn: &Connection) -> AppResult<()> {
     }
     Ok(())
 }
+#[cfg(test)]
+mod tests {
+    use core::num;
+    use std::{path::Path, time};
+
+    use rusqlite::backup;
+
+    use super::*;
+
+    fn setup_three_table_db() -> Result<Connection, rusqlite::Error> {
+        let conn = Connection::open_in_memory().unwrap();
+        conn.execute("CREATE TABLE t1 (id INTEGER PRIMARY KEY, c1 TEXT);", [])?;
+        conn.execute(
+            "CREATE TABLE t2 (id INTEGER PRIMARY KEY, c1 TEXT, c2 TEXT, c3 TEXT);",
+            [],
+        )?;
+        conn.execute(
+            "CREATE TABLE t3 (id INTEGER PRIMARY KEY, c1 TEXT, c2 TEXT, c3 TEXT, c4 INT);",
+            [],
+        )?;
+
+        for i in 0..5 {
+            let text = format!("lorem ipsum bla bla {}", i);
+            conn.execute(&(format!("INSERT INTO t1 (c1) VALUES ('{}')", text)), [])?;
+        }
+        for i in 3..8 {
+            let text1 = format!("lorem ipsum bla bla {}", i);
+            let text2 = format!("other {}", (i + 7) / 2);
+            let text3 = format!("last {}", (i + 19) / 3);
+            conn.execute(
+                &format!(
+                    "INSERT INTO t2 (c1, c2, c3) VALUES ('{}', '{}', '{}' )",
+                    text1, text2, text3
+                ),
+                [],
+            )?;
+        }
+        for i in 13..21 {
+            let text1 = format!("lorem ipsum bla bla {}", i);
+            let text2 = format!("other {}", (i + 7) / 2);
+            let text3 = format!("almost last {}", (i + 19) / 3);
+            let num = (i + 9) / 4;
+            conn.execute(
+                &format!(
+                    "INSERT INTO t3 (c1, c2,c3,c4) VALUES ('{}', '{}', '{}', '{}' )",
+                    text1, text2, text3, num
+                ),
+                [],
+            )?;
+        }
+
+        create_table_of_tables(&conn).unwrap();
+        populate_table_of_tables(&conn).unwrap();
+        Ok(conn)
+    }
+    fn write_db_to_disk(conn: &Connection) {
+        let dst = Path::new("test.db");
+        let mut dst = Connection::open(dst).unwrap();
+        let backup = backup::Backup::new(conn, &mut dst).unwrap();
+        backup
+            .run_to_completion(10, time::Duration::from_millis(250), None)
+            .unwrap();
+    }
+    #[test]
+    fn table_of_tables_num_count_test() {
+        let conn = setup_three_table_db().unwrap();
+        let num_tables = "SELECT COUNT(*) FROM table_of_tables";
+        let mut stmt = conn.prepare(num_tables).unwrap();
+        let count: i64 = stmt.query_row([], |row| row.get(0)).unwrap();
+        assert_eq!(count, 3);
+        write_db_to_disk(&conn);
+    }
+
+    #[test]
+    fn table_of_tables_int_col_count() {
+        let conn = setup_three_table_db().unwrap();
+        let num_tables_fun = |table| {
+            format!("SELECT int_col_count FROM table_of_tables WHERE table_name = '{table}'")
+        };
+        let mut stmt = conn.prepare(&num_tables_fun("t2")).unwrap();
+        let count: i64 = stmt.query_row([], |row| row.get(0)).unwrap();
+        assert_eq!(count, 0);
+        let mut stmt = conn.prepare(&num_tables_fun("t3")).unwrap();
+        let count: i64 = stmt.query_row([], |row| row.get(0)).unwrap();
+        assert_eq!(count, 1);
+        write_db_to_disk(&conn);
+    }
+
+    #[test]
+    fn table_of_tables_text_col_count() {
+        let conn = setup_three_table_db().unwrap();
+        let num_tables_fun = |table| {
+            format!("SELECT text_col_count FROM table_of_tables WHERE table_name = '{table}'")
+        };
+
+        let mut stmt = conn.prepare(&num_tables_fun("t1")).unwrap();
+        let count: i64 = stmt.query_row([], |row| row.get(0)).unwrap();
+        assert_eq!(count, 1);
+        let mut stmt = conn.prepare(&num_tables_fun("t2")).unwrap();
+        let count: i64 = stmt.query_row([], |row| row.get(0)).unwrap();
+        assert_eq!(count, 3);
+        let mut stmt = conn.prepare(&num_tables_fun("t3")).unwrap();
+        let count: i64 = stmt.query_row([], |row| row.get(0)).unwrap();
+        assert_eq!(count, 3);
+        write_db_to_disk(&conn);
+    }
+}
