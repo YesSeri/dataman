53eb825858cbc2d1a1e0dadb767a56bdceaa70b9
Henrik Zenkert <henrik.zenkert@gmail.com>
2024-08-05 10:21:28 +0200

    started working on benchmarking math

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,90 +1,51 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use dataman::model::regexping::custom_functions::add_custom_functions;
-use r2d2::Pool;
-use r2d2_sqlite::SqliteConnectionManager;
-use rusqlite::{Connection, Result};
-use std::{
-    thread::{self},
-    time::Instant,
-};
+use rusqlite::Connection;
+use rusqlite::Result;
+use std::time::Instant;
 
-fn setup_large_database(pool: &Pool<SqliteConnectionManager>, num_rows: usize) -> Result<()> {
-    let conn = pool.get().unwrap();
+fn setup_large_database(conn: &Connection, num_rows: usize) -> Result<()> {
     conn.execute(
-        "CREATE TABLE test (id INTEGER PRIMARY KEY, text TEXT, transformed_text TEXT)",
+        "CREATE TABLE test (id INTEGER PRIMARY KEY, val INT, transformed_val INT)",
         [],
     )?;
-    let mut stmt = conn.prepare("INSERT INTO test (text) VALUES (?)")?;
+    let mut stmt = conn.prepare("INSERT INTO test (val) VALUES (?)")?;
 
     for i in 0..num_rows {
-        let text = format!("Sample text with number {}", i);
-        stmt.execute([text])?;
+        let val = i;
+        stmt.execute([val])?;
     }
 
     Ok(())
 }
-
-use r2d2::CustomizeConnection;
-#[derive(Debug)]
-struct CustomFunctionAdder;
-
-impl CustomizeConnection<Connection, rusqlite::Error> for CustomFunctionAdder {
-    fn on_acquire(&self, conn: &mut Connection) -> Result<(), rusqlite::Error> {
-        add_custom_functions(conn)
-    }
-}
-
-fn benchmark_regexp_transform_no_capture_group(c: &mut Criterion) {
-    let manager = SqliteConnectionManager::memory();
-
-    let pool = Pool::builder()
-        .max_size(4)
-        .connection_customizer(Box::new(CustomFunctionAdder))
-        .build(manager)
-        .unwrap();
-
-    let num_rows = 1000000;
-    setup_large_database(&pool, num_rows).unwrap();
-
-    c.bench_function("regexp transform single-threaded", |b| {
+fn benchmark_math(c: &mut Criterion) {
+    let conn0 = Connection::open_in_memory().unwrap();
+    setup_large_database(&conn0, 100000).unwrap();
+    let conn1 = Connection::open_in_memory().unwrap();
+    setup_large_database(&conn1, 100000).unwrap();
+    c.bench_function("math sql query", |b| {
         b.iter(|| {
-            let conn = pool.get().unwrap();
-            let regex_str = r"\d+";
-            let mut stmt = conn
-                .prepare(
-                    "UPDATE test SET transformed_text = regexp_transform_no_capture_group(?, text)",
-                )
+            let start = Instant::now();
+            let mut stmt = conn0
+                .prepare("UPDATE test SET transformed_val = ?")
                 .unwrap();
-            stmt.execute([regex_str]).unwrap();
+            let math_expr = "val + 1";
+            stmt.execute([math_expr]).unwrap();
+            start.elapsed()
         })
     });
 
-    c.bench_function("regexp transform multi-threaded", |b| {
-        b.iter_custom(|iters| {
+    c.bench_function("math compiled function", |b| {
+        b.iter(|| {
             let start = Instant::now();
-            let handles: Vec<_> = (0..4)
-                .map(|i| {
-                    let pool = pool.clone();
-                    let start = i * (num_rows / 4) + 1; // +1 to start from 1 since id starts from 1
-                    let end = if i == 3 { num_rows } else { start + (num_rows / 4) - 1 };
-                    thread::spawn(move || {
-                        for _ in 0..iters {
-                            let conn = pool.get().unwrap();
-                            let regex_str = r"\d+";
-                            let mut stmt = conn.prepare("UPDATE test SET transformed_text = regexp_transform_no_capture_group(?, text) WHERE id BETWEEN ? AND ?").unwrap();
-                            stmt.execute((regex_str, start, end)).unwrap();
-                        }
-                    })
-                })
-                .collect();
-
-            for handle in handles {
-                handle.join().unwrap();
-            }
+            let mut stmt = conn1
+                .prepare("UPDATE test SET transformed_val = ?")
+                .unwrap();
+            let math_expr = "val / 7 + 2 ";
+            stmt.execute([math_expr]).unwrap();
             start.elapsed()
         })
     });
 }
 
-criterion_group!(benches, benchmark_regexp_transform_no_capture_group);
+criterion_group!(benches, benchmark_math);
 criterion_main!(benches);
